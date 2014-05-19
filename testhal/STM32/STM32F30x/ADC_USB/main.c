212d08f5c4eaecf3f1d8387595f73342dea10c36
Austin Glaser <austin.glaser@gmail.com>
2014-05-18 18:15:40 -0600

    add newline every 10 samples to adc callback

--- testhal/STM32/STM32F30x/ADC_USB/main.c
+++ testhal/STM32/STM32F30x/ADC_USB/main.c
@@ -437,7 +437,7 @@ static void cmd_adc(BaseSequentialStream *chp, int argc, char * argv[]) {
   int i;
   for (i = 0; i < ADC_GRP2_NUM_CHANNELS * ADC_GRP2_BUF_DEPTH; i++) {
     chprintf(chp, "%03d: %04u ", i, samples2[i]);
-    //if (i%10 == 0) chprintf(chp, "\r\n");
+    if (i%10 == 0) chprintf(chp, "\r\n");
   }
   chprintf(chp, "\r\n\r\n");
 }
